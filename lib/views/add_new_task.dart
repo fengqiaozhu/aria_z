905b6d3cc797d148cbddeec396d4d872954779b0
fq <815930932@qq.com>
2021-11-29 00:18:46 +0800

    add file_picker to addNewTask view;

--- lib/views/add_new_task.dart
+++ lib/views/add_new_task.dart
@@ -1,13 +1,65 @@
 // ignore_for_file: must_be_immutable
 
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import '../states/app.dart';
 import '../components/custom_snack_bar.dart';
 
+class DownloadSourceFile2Base64 {
+  String content2Base64;
+
+  String name;
+
+  String path;
+
+  DownloadSourceFile2Base64(this.name, this.path, this.content2Base64);
+}
+
 class AddNewAria2Task extends StatelessWidget {
   late Aria2TaskType taskType;
   AddNewAria2Task({Key? key}) : super(key: key);
+
+  readDownloadSourceFileToBase64(FilePickerResult fileResult) {
+    return fileResult.names
+        .asMap()
+        .keys
+        .map((idx) => DownloadSourceFile2Base64(
+            fileResult.names[idx] ?? '',
+            fileResult.paths[idx] ?? '',
+            base64Encode(fileResult.files[idx].bytes!)))
+        .toList();
+  }
+
+  Widget showAddWidgetByTaskType() {
+    late Widget w;
+    switch (taskType.taskType) {
+      case TaskType.torrent:
+        w = TextButton(
+            onPressed: () async {
+              FilePickerResult? result =
+                  // await FilePicker.platform.pickFiles(allowMultiple: true, type: FileType.custom, allowedExtensions: ['torrent','zip']);
+                  await FilePicker.platform.pickFiles(
+                      type: FileType.any, allowMultiple: true, withData: true);
+              if (result != null) {
+                List<DownloadSourceFile2Base64> tmp =
+                    readDownloadSourceFileToBase64(result);
+                print(tmp);
+              } else {
+                // User canceled the picker
+              }
+            },
+            child: const Text("选择种子文件"));
+        break;
+      default:
+        w = const Text('添加新任务');
+        break;
+    }
+    return w;
+  }
 
   _submitNewTask(BuildContext context) {
     showCustomSnackBar(context, 1, const Text('添加任务成功'));
@@ -28,7 +80,7 @@ class AddNewAria2Task extends StatelessWidget {
         ],
       ),
       body: Container(
-        child: const Text('添加新任务'),
+        child: showAddWidgetByTaskType(),
       ),
     );
   }
