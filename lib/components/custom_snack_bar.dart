827681e733e29563d46d8fe014479a5900605bc3
fq <815930932@qq.com>
2021-12-01 20:09:50 +0800

    add custom SnackBar duration

--- lib/components/custom_snack_bar.dart
+++ lib/components/custom_snack_bar.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
+/// 底部提醒弹框
 /// level = 1:成功，2：失败，3：警告
-void showCustomSnackBar(BuildContext context, int level, Widget content) {
+void showCustomSnackBar(BuildContext context, int level, Widget content,
+    {int durationSecond = 1}) {
   Color bg = Colors.grey;
 
   switch (level) {
@@ -18,7 +20,7 @@ void showCustomSnackBar(BuildContext context, int level, Widget content) {
 
   ScaffoldMessenger.of(context).showSnackBar(
     SnackBar(
-      duration: const Duration(seconds: 1),
+      duration: Duration(seconds: durationSecond),
       content: content,
       backgroundColor: bg,
     ),
