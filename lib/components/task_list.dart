12ea3006f8fbcc93787aef6bdacac4610dba1677
fq <815930932@qq.com>
2021-12-05 21:49:33 +0800

    fix types task show in list bug;

--- lib/components/task_list.dart
+++ lib/components/task_list.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 
 import '../states/app.dart' show AppState;
-import '../states/aria2.dart' show Aria2States;
+import '../states/aria2.dart' show Aria2States, TaskType;
 import '../utils/tools.dart';
 
 List<Widget> taskListTileWidget(BuildContext context, AppState app,
@@ -49,8 +49,28 @@ List<Widget> taskListTileWidget(BuildContext context, AppState app,
   }
 
   return taskList
-      .where((element) => element.bittorrent?["info"] != null)
+      // .where((element) => element.bittorrent?["info"] != null)
       .map((task) {
+    late TaskType _taskType;
+    late String _taskName;
+
+    if (task.bittorrent == null) {
+      String uri = task.files?[0]['uris'][0]['uri'] as String;
+      _taskType = TaskType.url;
+      _taskName = uri.split('/').last;
+    } else if (task.bittorrent != null) {
+      if (task.bittorrent!.containsKey('info')) {
+        _taskType = TaskType.torrent;
+        _taskName = task.bittorrent?["info"]["name"] ?? '';
+      } else {
+        _taskType = TaskType.magnet;
+        _taskName = '[METADATA]${task.infoHash}';
+      }
+    } else {
+      _taskType = TaskType.metaLink;
+      _taskName = '==========metaLink============';
+    }
+
     return Padding(
       padding: const EdgeInsets.all(10.0),
       child: Card(
@@ -76,32 +96,92 @@ List<Widget> taskListTileWidget(BuildContext context, AppState app,
                       var gid = task.gid ?? '';
                       if (gid != "") {
                         Aria2Task? ts = await app.aria2?.tellStatus(gid);
-                        Navigator.of(context)
-                            .pushNamed('/task_detail', arguments: ts);
+                        Navigator.of(context).pushNamed('/task_detail',
+                            arguments: [ts, _taskName, _taskType]);
                       }
                     },
+                    leading: Icon(app.aria2TaskTypes
+                        .where((att) => att.taskType == _taskType)
+                        .first
+                        .icon),
                     title: Text(
-                      task.bittorrent?["info"]["name"],
+                      _taskName,
                       overflow: TextOverflow.ellipsis,
                       maxLines: 2,
                       // softWrap: false,
                     ),
                     trailing:
                         trailingOption(task.gid ?? '', task.status ?? ''))),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                Row(children: <Widget>[
-                  const Icon(Icons.upload),
-                  Text(formatSpeed(task.uploadSpeed ?? 0))
-                ]),
-                const SizedBox(width: 8),
-                Row(children: <Widget>[
-                  const Icon(Icons.download),
-                  Text(formatSpeed(task.downloadSpeed ?? 0))
-                ])
-              ],
-            ),
+            Padding(
+                padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
+                child: Flex(
+                  direction: Axis.horizontal,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                        child: Text(
+                      "${formatFileSize(task.completedLength ?? 0)} / ${formatFileSize(task.totalLength ?? 0)}",
+                      style: TextStyle(
+                          color: Theme.of(context).colorScheme.onSurface,
+                          fontSize: 14,
+                          fontFamily: 'Coda'),
+                    )),
+                    Expanded(
+                        flex: 1,
+                        child: DefaultTextStyle(
+                            textAlign: TextAlign.end,
+                            style: TextStyle(
+                                color: Theme.of(context).colorScheme.onSurface,
+                                fontSize: 14,
+                                fontFamily: 'Coda'),
+                            child: Text.rich(TextSpan(
+                              children: [
+                                task.status == 'paused'
+                                    ? const WidgetSpan(child: Text('已暂停'))
+                                    : TextSpan(children: [
+                                        const WidgetSpan(
+                                            child: Icon(
+                                          Icons.file_download_outlined,
+                                          size: 18,
+                                          color: Colors.green,
+                                        )),
+                                        TextSpan(
+                                            text: formatSpeed(
+                                                task.downloadSpeed ?? 0)),
+                                      ]),
+                                _taskType != TaskType.torrent
+                                    ? const WidgetSpan(child: SizedBox())
+                                    : const WidgetSpan(
+                                        child: SizedBox(
+                                        height: 16,
+                                        child: VerticalDivider(
+                                          color: Colors.grey,
+                                          thickness: 2,
+                                          width: 20,
+                                        ),
+                                      )),
+                                // const TextSpan(
+                                //     text: '     ',
+                                //     style: TextStyle(
+                                //         fontWeight:
+                                //             FontWeight.w800)),
+                                _taskType != TaskType.torrent
+                                    ? const WidgetSpan(child: SizedBox())
+                                    : TextSpan(children: [
+                                        const WidgetSpan(
+                                            child: Icon(
+                                          Icons.file_upload_outlined,
+                                          size: 18,
+                                          color: Colors.red,
+                                        )),
+                                        TextSpan(
+                                            text: formatSpeed(
+                                                task.uploadSpeed ?? 0)),
+                                      ])
+                              ],
+                            ))))
+                  ],
+                )),
             LinearProgressIndicator(
                 value: (task.completedLength ?? 0) /
                     (task.totalLength == 0 ? 1 : task.totalLength ?? 1)),
